a103f965788e0e2ff72d467c12d749daec9923de
pinkforest <36498018+pinkforest@users.noreply.github.com>
2022-06-26 18:40:57 +1000

    udp sockets steady growth

--- src/bin/udp_ping_pong.rs
+++ src/bin/udp_ping_pong.rs
@@ -17,14 +17,19 @@ fn send_ping(_: String, _: Mailbox<()>) {
     let local_addr = socket.local_addr().unwrap();
     println!("send_ping<{}> UdpSocket bound", &local_addr);    
     socket.connect("127.0.0.1:8888").expect("connect function failed");
-    socket.send("PING".as_bytes()).expect("couldn't send message");
-    println!("send_ping<{}> PING Sent", &local_addr);
 
-    let mut buf = [0; 4];
-    match socket.recv(&mut buf) {
-        Ok(received) => println!("send_ping<{}> PONG {received} bytes {:?}", &local_addr, &buf[..received]),
-        Err(e) => println!("send_ping<{}> PONG recv function failed: {e:?}", &local_addr),
-    }    
+    loop {
+        socket.send("PING".as_bytes()).expect("couldn't send message");
+        println!("send_ping<{}> PING Sent", &local_addr);
+        
+        let mut buf = [0; 4];
+        match socket.recv(&mut buf) {
+            Ok(received) => println!("send_ping<{}> PONG {received} bytes {:?}", &local_addr, &buf[..received]),
+            Err(e) => println!("send_ping<{}> PONG recv function failed: {e:?}", &local_addr),
+        }
+        
+        sleep(Duration::from_millis(1000));
+    }
 }
 
 fn wait_ping(_: String, _: Mailbox<()>) {
