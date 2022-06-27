da84475092c328d7e326caaf2a8f347e265e69b1
pinkforest <36498018+pinkforest@users.noreply.github.com>
2022-06-28 06:46:54 +1000

    cleanup

--- src/bin/udp_ping_pong.rs
+++ src/bin/udp_ping_pong.rs
@@ -14,19 +14,14 @@ fn main(_: Mailbox<()>) {
 
 fn send_ping(_: String, _: Mailbox<()>) {
     let socket = net::UdpSocket::bind("127.0.0.1:0").unwrap();
-    let local_addr = socket.local_addr().unwrap();
-    //println!("send_ping<{}> UdpSocket bound", &local_addr);    
+    let _local_addr = socket.local_addr().unwrap();
+
     socket.connect("127.0.0.1:8888").expect("connect function failed");
 
     loop {
-
         socket.send("PING".as_bytes()).expect("couldn't send message");
-        //println!("send_ping<{}> PING Sent", &local_addr);
-        
         let mut buf = [0; 4];
-        socket.recv(&mut buf).expect("recv error");;
-        // println!("send_ping<{}> PONG {received} bytes {:?}", &local_addr, &buf[..received]);
-        
+        socket.recv(&mut buf).expect("recv error");
         sleep(Duration::from_millis(1000));
     }
 }
@@ -34,15 +29,11 @@ fn send_ping(_: String, _: Mailbox<()>) {
 fn wait_ping(_: String, _: Mailbox<()>) {
 
     let socket = net::UdpSocket::bind("127.0.0.1:8888").unwrap();
-    let local_addr = socket.local_addr().unwrap();
-    //println!("wait_ping<{}>> UdpSocket bound", &local_addr);
+    let _local_addr = socket.local_addr().unwrap();
     
     loop {
         let mut buf = [0; 4];
-        let (len, addr) = socket.recv_from(&mut buf).unwrap();
-        //println!("wait_ping<{}> UdpSocket received {:?} bytes from {:?}", &local_addr, &len, &addr);
-
-        let len_out = socket.send_to("PONG".as_bytes(), addr).unwrap();
-        //println!("wait_ping<{}> UdpSocket replied PONG {:?} bytes to {:?}", &local_addr, &len_out, &addr);        
+        let (_len, addr) = socket.recv_from(&mut buf).unwrap();
+        let _len_out = socket.send_to("PONG".as_bytes(), addr).unwrap();
     };
 }
